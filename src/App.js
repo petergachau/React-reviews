a7a93c0b83ab8744a29be31c69cba0709486ae4d
petergachau <petergachau57@gmail.com>
2022-02-09 09:41:23 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Review from './Review';
+function App() {
+  return (
+    <section className="container">
+      <div className="title">
+      <h1>Our reviews</h1>
+        <div className="underline"></div>
+      </div>
+      <Review/>
+    </section>
+  );
+}
+
+export default App;
